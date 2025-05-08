efa730b9deddcedfab48c8f02aaeb50df6292a29
Ehsan Fani <ehsan.fani@emeal.nttdata.com>
2025-05-08 15:39:27 +0200

    added signal and observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  private destroyRef = inject(DestroyRef);
 
+  constructor() {
+    effect(() => {
+      console.log(`Clickec button ${this.clickCount()} times.`);
+    });
+  }
+  ngOnInit(): void {
+    //observable
+    // const subscription = interval(1000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe({
+    //     next: (val) => console.log(val),
+    //   });
+
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+  }
+  //signal
+  onClick() {
+    this.clickCount.update((preVal) => preVal + 1);
+  }
 }
